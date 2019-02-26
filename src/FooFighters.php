2b28f27e9050484278718ef5f6613b7eb9eea51e
Tyler Oscarson <tyler@theshortb.us>
2019-02-25 18:40:57 -0700

    Adding foofighters in src

---
+++ src/FooFighters.php
@@ -0,0 +1,12 @@
+<?php
+class FooFighters {
+     public $foo;
+     public __construct(){
+          $this->foo = new array();
+          $this->foo[] = 'foo';
+     }
+     public getFoo() {
+          $random = random_int(0, count($this->foo)-1);
+          return $this->foo[$random];
+     }
+}
